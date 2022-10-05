a179d85229dc8a1afe85f879f50477c725b156b7
Clowiser <jessica-rigaud@hotmail.fr>
2022-10-05 19:19:16 +0200

    feat(Server) : finish to build Node.js Rest API & display mySQL data

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 const express = require("express");
 const app = express()
 const mysql = require('mysql2');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Serveur en écoute sur le port 3000')
@@ -22,10 +27,39 @@ connection.connect(function (err) {
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(cors())
 
-app.get('/', function (req, res) {
-    console.log(req);
-    connection.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'portfoliodatabase'", function (error, result, fields) {
+app.get(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.get('/api/realisation_home', function (req, res, next) {
+    connection.query("SELECT * FROM portfoliodatabase.realisation_home", function (error, result, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(result));
+    });
+});
+
+app.get('/api/portfolio_web_realisations', function (req, res, next) {
+    connection.query("SELECT * FROM portfoliodatabase.portfolio_web_realisations", function (error, result, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(result));
+    });
+});
+
+app.get('/api/portfolio_graphic_digital_creations', function (req, res, next) {
+    connection.query("SELECT * FROM portfoliodatabase.portfolio_graphic_digital_creations", function (error, result, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(result));
+    });
+});
+
+app.get('/api/portfolio_graphic_3d', function (req, res, next) {
+    connection.query("SELECT * FROM portfoliodatabase.portfolio_graphic_3d", function (error, result, fields) {
         if (error) throw error;
         res.end(JSON.stringify(result));
     });
